ce2ba6474453c10330099253c346448d9ce799b1
suzuki-tatsuki <suzuki.tatsuki.20@shizuoka.ac.jp>
2024-08-29 22:09:46 +0900

    [feat] create message.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+mod message;
+
 use dotenvy::dotenv;
 use std::env;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     dotenv().expect(".env file not loaded");
     let discord_token = env::var("DISCORD_TOKEN").expect("discord_token not found");
+
+    message::send().await;
 }
