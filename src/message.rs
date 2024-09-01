f1a5fff76dd56e24268798f061b3aed216b3e457
suzuki-tatsuki <suzuki.tatsuki.20@shizuoka.ac.jp>
2024-09-01 19:00:49 +0900

    [refactor] cahnged context of sending message

--- src/message.rs
+++ src/message.rs
@@ -46,10 +46,10 @@ impl EventHandler for Handler {
                             CreateEmbed::new()
                                 .color(0x0000FF)
                                 .title("player1の得点")
-                                .field("得点", my_score.to_string(), true)
-                                .field("player2との差", diff1_2.to_string(), false)
-                                .field("player3との差", diff1_3.to_string(), false)
-                                .field("player4との差", diff1_4.to_string(), false)
+                                .field("得点", my_score.to_string(), false)
+                                .field("p2との差", diff1_2.to_string(), true)
+                                .field("p3との差", diff1_3.to_string(), true)
+                                .field("p4との差", diff1_4.to_string(), true)
                         )
                 ).await {
                     println!("Error sending message: {:?}", why);
@@ -67,9 +67,9 @@ impl EventHandler for Handler {
                                 .color(0xDC143C)
                                 .title("player2の得点")
                                 .field("得点", my_score.to_string(), true)
-                                .field("player2との差", diff2_1.to_string(), false)
-                                .field("player3との差", diff2_3.to_string(), false)
-                                .field("player4との差", diff2_4.to_string(), false)
+                                .field("p2との差", diff2_1.to_string(), false)
+                                .field("p3との差", diff2_3.to_string(), false)
+                                .field("p4との差", diff2_4.to_string(), false)
                         )
                 ).await {
                     println!("Error sending message: {:?}", why);
@@ -87,9 +87,9 @@ impl EventHandler for Handler {
                                 .color(0x008000)
                                 .title("player1の得点")
                                 .field("得点", my_score.to_string(), true)
-                                .field("player2との差", diff3_1.to_string(), false)
-                                .field("player3との差", diff3_2.to_string(), false)
-                                .field("player4との差", diff3_4.to_string(), false)
+                                .field("p2との差", diff3_1.to_string(), false)
+                                .field("p3との差", diff3_2.to_string(), false)
+                                .field("p4との差", diff3_4.to_string(), false)
                         )
                 ).await {
                     println!("Error sending message: {:?}", why);
@@ -107,9 +107,9 @@ impl EventHandler for Handler {
                                 .color(0xFFFF00)
                                 .title("player4の得点")
                                 .field("得点", my_score.to_string(), true)
-                                .field("player2との差", diff4_1.to_string(), false)
-                                .field("player3との差", diff4_2.to_string(), false)
-                                .field("player4との差", diff4_3.to_string(), false)
+                                .field("p2との差", diff4_1.to_string(), false)
+                                .field("p3との差", diff4_2.to_string(), false)
+                                .field("p4との差", diff4_3.to_string(), false)
                         )
                 ).await {
                     println!("Error sending message: {:?}", why);
