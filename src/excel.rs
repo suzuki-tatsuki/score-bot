14512db4ba3ecfcdb33b55abd256b87018f27d0d
suzuki-tatsuki <suzuki.tatsuki.20@shizuoka.ac.jp>
2024-09-12 09:15:39 +0900

    [refactor] change excel file to read

--- src/excel.rs
+++ src/excel.rs
@@ -7,14 +7,14 @@ pub struct ExcelData {
 }
 
 pub fn read_excel() -> ExcelData {
-    let xl_book_path = "data.xlsx";
+    let xl_book_path = "Suzuki_League_Score.xlsx";
     let mut workbook: Xlsx<_> = open_workbook(xl_book_path).expect("cannot open xl book");
 
     let mut score = vec![0.0, 0.0, 0.0, 0.0];
 
     if let Ok(range) = workbook.worksheet_range("Sheet1") {
         for row in 1..5 {
-            let cell_value = range.get_value((row as u32, 8));
+            let cell_value = range.get_value((row as u32, 11));
 
 
             match cell_value {
